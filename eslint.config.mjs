67f798717d553427e95c7dfe668c8380bf280f7a
Defuse Code <Mohammed_Abdelhady97@hotmail.com>
2024-06-28 22:13:22 +0300

    setup: eslint with prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: 'detect', // Automatically detects the react version
+      },
+    },
+  },
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off', // Disable the rule
+    },
+  },
+];
