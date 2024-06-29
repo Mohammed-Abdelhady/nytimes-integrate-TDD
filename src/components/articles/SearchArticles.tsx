b73f4b51eb858581cd87431af4d8e51a68d8068d
Defuse Code <Mohammed_Abdelhady97@hotmail.com>
2024-06-29 07:58:11 +0300

    fix: theme bg for search article

--- src/components/articles/SearchArticles.tsx
+++ src/components/articles/SearchArticles.tsx
@@ -1,8 +1,10 @@
-import { Input } from '@chakra-ui/react';
+import { Input, useColorMode } from '@chakra-ui/react';
 import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 const SearchArticles = () => {
+  const { colorMode } = useColorMode();
+
   const [searchParams, setSearchParams] = useSearchParams();
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newSearchParams = new URLSearchParams(searchParams);
@@ -13,7 +15,7 @@ const SearchArticles = () => {
     <Input
       mb="5"
       placeholder={'Enter search term'}
-      bg="gray.100"
+      bg={colorMode == 'light' ? 'gray.100' : 'gray.700'}
       p="1"
       borderRadius={'md'}
       onChange={handleSearch}
