af8f07510c3758290953e1c160e3e615cbdb9e1d
Defuse Code <Mohammed_Abdelhady97@hotmail.com>
2024-06-29 07:09:02 +0300

    feat: article sections & filter articles based on section

---
+++ src/components/articles/ArticlesSections.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Box, Heading, ListItem, UnorderedList } from '@chakra-ui/react';
+import { useFetchAllSectionsQuery } from 'lib/slices/articlesSlice';
+import { useSearchParams } from 'react-router-dom';
+
+/**
+ * Displays a list of article sections fetched from the API.
+ */
+const ArticlesSections = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { data: sections } = useFetchAllSectionsQuery({
+    period: Number(searchParams.get('period')) || 1,
+  });
+
+  /**
+   * Handles click event on a section item.
+   * Updates URL search params with the selected section.
+   * @param {string} section - The section name clicked.
+   */
+  const handleSectionClick = (section: string) => {
+    setSearchParams(new URLSearchParams({ section }));
+  };
+
+  return (
+    <Box>
+      <Heading size="md">Sections</Heading>
+      <UnorderedList styleType="none" width="max-content">
+        {sections?.map((section) => (
+          <ListItem
+            as="button"
+            display="block"
+            mt="3"
+            onClick={() => handleSectionClick(section)}
+            key={section}>
+            {section}
+          </ListItem>
+        ))}
+      </UnorderedList>
+    </Box>
+  );
+};
+
+export default ArticlesSections;
--- src/pages/articles/ArticlesListPage.tsx
+++ src/pages/articles/ArticlesListPage.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useFetchArticlesQuery } from '../../lib/slices/articlesSlice';
+import ArticlesSections from 'components/articles/ArticlesSections';
+import { Box, Flex, SimpleGrid, Text } from '@chakra-ui/react';
+import { useSearchParams } from 'react-router-dom';
 
 const ArticlesListPage = () => {
-  return <div>ArticlesListPage</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const {
+    data: articles,
+    error,
+    isLoading,
+  } = useFetchArticlesQuery({
+    period: Number(searchParams.get('period')) || 1,
+    search: '',
+    section: searchParams.get('section') || '',
+  });
+  console.log(error, articles, 'articles');
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Something Went Wrong</div>;
+  return (
+    <Flex>
+      <ArticlesSections />
+      <SimpleGrid columns={[1, 2, 3]} gap={4} ml="10">
+        {articles?.map((article) => (
+          <Box p={4} bg="blue.500" color="white" key={article.id}>
+            <Text>{article.section}</Text>
+            <Text>{article.title}</Text>
+          </Box>
+        ))}
+      </SimpleGrid>
+    </Flex>
+  );
 };
 
 export default ArticlesListPage;
